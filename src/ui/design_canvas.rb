acc6261a42ea04aa4c60b7cf2e1660a2b00552ee
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-25 23:07:41 +0000

    Add basic selection support for DesignCanvas

--- src/ui/design_canvas.rb
+++ src/ui/design_canvas.rb
@@ -6,7 +6,10 @@ module LibreFrame
   module UI
     # A canvas on which designs are displayed.
     class DesignCanvas < Gtk::DrawingArea
-      attr_accessor :view
+      attr_accessor :view, :selection
+
+      DEBUG_POINT_COLOR = [1, 0, 0]
+      SELECTION_BOX_COLOR = [0, 0, 1, 0.4]
 
       def initialize
         super
@@ -25,12 +28,13 @@ module LibreFrame
 
         signal_connect 'button-press-event' do |_, button_event|
           # "Ask" higher-level elements about clicks first
-          clicked_element = @elements.flat_map(&:onedimensionalize).reverse.find do |el|
+          clicked_element = elements.flat_map(&:onedimensionalize).reverse.find do |el|
             point = Core::Point.new(button_event.x, button_event.y)
             el.contains_position?(point, view) 
           end
 
-          p clicked_element
+          @selection = clicked_element
+          queue_draw
         end
 
         set_events :button_press_mask
@@ -46,13 +50,24 @@ module LibreFrame
       def draw
         ctx = window.create_cairo_context
         
+        # Draw elements
         @elements.each do |element|
           element.cairo_draw(ctx, view)
         end
 
+        # Draw selection bounding box
+        unless selection.nil?
+          ctx.set_source_rgba(*SELECTION_BOX_COLOR)
+          # TODO: There REALLY needs to be a method for view.tp + offset
+          pos = view.tp(selection.position) + selection.offset
+          ctx.rectangle(pos.x, pos.y, selection.width, selection.height)
+          ctx.fill
+        end
+
+        # Draw debug points, if debug mode enabled
         if view.debug?
           view.debug_points.each do |point|
-            ctx.set_source_rgb(1, 0, 0)
+            ctx.set_source_rgb(*DEBUG_POINT_COLOR)
             ctx.rectangle(point.x, point.y, 1, 1)
             ctx.fill
           end
