3848c423ade9d58c600d94e4881908277e97fa04
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-25 22:49:59 +0000

    Add (seemingly broken) perp isct method

--- src/core/geometry.rb
+++ src/core/geometry.rb
@@ -11,7 +11,33 @@ module LibreFrame
         delta_x = (e.x - s.x) / divisor
         delta_y = (e.y - s.y) / divisor
 
-        Core::Point.new(e.x + delta_x * delta, e.y + delta_y * delta)
+        Point.new(e.x + delta_x * delta, e.y + delta_y * delta)
+      end
+
+      # Calculates the intersection of the perpendiculars of two lines.
+      # The lines are specified by two points each:
+      # - The pivot point, which is where the line is "rotated" around to obtain
+      #   the perpendicular.
+      # - The guide point, which is used to calculate the gradient alongside the
+      #   pivot point.
+      def self.perpendicular_intersects(first_pivot, first_guide, second_pivot, second_guide)
+        # Calculate gradients
+        first_grad = (first_pivot.y - first_guide.y)/(first_pivot.x - first_guide.x)
+        second_grad = (second_pivot.y - second_guide.y)/(second_pivot.x - second_guide.x)
+
+        # Calculate perpendicular gradients
+        first_perp_grad = -1 / first_grad
+        second_perp_grad = -1 / second_grad
+
+        # Find y intersects
+        first_c = -first_perp_grad * first_pivot.x + first_pivot.y
+        second_c = -second_perp_grad * second_pivot.x + second_pivot.y
+
+        # Find intersect x and y
+        isct_x = (first_c - second_c) / (second_perp_grad - first_perp_grad)
+        isct_y = second_c + second_perp_grad * isct_x
+
+        Point.new(isct_x, isct_y)
       end
     end
   end
