8f847f66a86f967a523763236ade5d706afd29e1
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-25 18:54:47 +0000

    Remove dead code comment

--- src/element_framework/rectangle.rb
+++ src/element_framework/rectangle.rb
@@ -7,33 +7,5 @@ module LibreFrame
     # they can also be modified just like a shape path so it makes more sense
     # to treat them as such.
     class Rectangle < ShapePath; end
-
-    <<-HERE
-    # An old-style rectangle.
-    class OldRectangle < StyledElement
-      def initialize
-        super
-      end
-
-      def cairo_draw(ctx, view)
-        translated_point = view.tp(position) + offset #relative_to.position
-
-        ctx.rectangle(translated_point.x, translated_point.y, view.sl(width), view.sl(height))
-
-        cairo_draw_styles(ctx, view)
-        cairo_draw_children(ctx, view)
-      end
-
-      def contains_position?(click_point, view)
-        o = view.tp(position) + offset
-        w = view.sl(width)
-        h = view.sl(height)
-
-        p click_point
-
-        click_point.x >= o.x && click_point.y >= o.y && click_point.x <= o.x + w && click_point.y <= o.y + h
-      end
-    end
-    HERE
   end
 end
