9f809f8f3e94418289a683dd407d2cb5fa760eb9
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-26 10:29:35 +0000

    Add Color class

---
+++ src/core/color.rb
@@ -0,0 +1,21 @@
+module LibreFrame
+  module Core
+    # Represents an RGBA color, where each value is a decimal between 0 and 1.
+    class Color
+      attr_accessor :r, :g, :b, :a
+
+      def initialize(r, g, b, a)
+        @r = r
+        @g = g
+        @b = b
+        @a = a
+      end
+
+      # Converts this color into an array which may be splatted into
+      # Cairo::Context#set_source_rgba.
+      def to_cairo
+        [r, g, b, a]
+      end
+    end
+  end
+end
