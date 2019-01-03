986b9d7510d9b1667ad46f06c4523e4489c243c0
Aaron Christiansen <aaronc20000@gmail.com>
2019-01-03 22:33:38 +0000

    Remove dead method

--- src/ui/toolbox.rb
+++ src/ui/toolbox.rb
@@ -30,66 +30,6 @@ module LibreFrame
           end
         end
       end
-
-      def element_for_property(element, prop)
-        # If the property requires an argument, it must be a view
-        if element.method(prop).arity == 1
-          prop_val = element.send(prop, element.view)
-        else
-          prop_val = element.send(prop)
-        end
-        p prop_val.class
-
-        property_box = Gtk::Box.new(:vertical, 1)
-        property_box.add_child(Gtk::Label.new(prop))
-
-        setter = (prop.to_s + '=').to_sym
-
-        if prop_val.is_a?(String) || prop_val.is_a?(Numeric)
-          entry = Gtk::Entry.new
-          entry.text = prop_val.to_s
-          handler = Proc.new do
-            if prop_val.is_a?(String)
-              element.send(setter, entry.text)
-            elsif prop_val.is_a?(Integer)
-              element.send(setter, entry.text.to_i)
-            elsif prop_val.is_a?(Float)
-              element.send(setter, entry.text.to_f)
-            end
-
-            canvas.queue_draw
-          end
-          entry.signal_connect 'changed', &handler
-
-          property_box.add_child(entry)
-
-        elsif prop_val.is_a?(Core::Point)
-          x_entry = Gtk::Entry.new
-          y_entry = Gtk::Entry.new
-
-          x_entry.text = prop_val.x.to_s
-          x_entry.signal_connect 'changed' do
-            element.send(setter, Core::Point.new(x_entry.text.to_f, y_entry.text.to_f))
-
-            canvas.queue_draw
-          end
-          property_box.add_child(x_entry)
-
-          y_entry.text = prop_val.y.to_s
-          y_entry.signal_connect 'changed' do
-            element.send(setter, Core::Point.new(x_entry.text.to_f, y_entry.text.to_f))
-
-            canvas.queue_draw
-          end
-          property_box.add_child(y_entry)
-        # TODO: Boolean?
-        else
-          property_box.add_child(Gtk::Label.new("Can't interpret #{prop_val.class}: #{prop_val}"))
-        end
-
-        property_box.show_all
-        property_box
-      end
     end
   end
 end
