e7a5f03a56cad86492db5e22f21227e5c14764f7
Aaron Christiansen <aaronc20000@gmail.com>
2019-01-03 23:36:46 +0000

    Add CurvePoint#has_curve_to?/from?

--- src/element_framework/curve_point.rb
+++ src/element_framework/curve_point.rb
@@ -8,6 +8,7 @@ module LibreFrame
     # those points.
     class CurvePoint
       attr_accessor :curve_from, :curve_to, :point, :corner_radius
+      bool_accessor :has_curve_from, :has_curve_to
 
       def initialize
         # TODO: Discards much info
@@ -20,6 +21,8 @@ module LibreFrame
       def from_sketch_json_hash(hash, loader)
         @curve_from = loader.string_to_point(hash['curveFrom'])
         @curve_to = loader.string_to_point(hash['curveTo'])
+        @has_curve_from = hash['hasCurveFrom']
+        @has_curve_to = hash['hasCurveTo']
         @point = loader.string_to_point(hash['point'])
         @corner_radius = hash['cornerRadius'].to_i
       end
