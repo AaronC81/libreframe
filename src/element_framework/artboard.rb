5b8b14342b1b7660173484858fad4a1856e9f184
Aaron Christiansen <aaronc20000@gmail.com>
2019-01-03 22:22:33 +0000

    Remove styling from artboards (was causing segfaults)

--- src/element_framework/artboard.rb
+++ src/element_framework/artboard.rb
@@ -15,12 +15,8 @@ module LibreFrame
         abs_pos = absolute_position
         ctx.rectangle(abs_pos.x, abs_pos.y, view.sl(width), view.sl(height))
 
-        if fills.empty?
-          ctx.set_source_rgba(*DEFAULT_BACKGROUND_COLOR)
-          ctx.fill_preserve
-        else
-          cairo_draw_styles(ctx)
-        end
+        ctx.set_source_rgba(*DEFAULT_BACKGROUND_COLOR)
+        ctx.fill_preserve
 
         cairo_draw_children(ctx)
       end
