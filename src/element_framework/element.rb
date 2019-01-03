2968b5de024719b375d940d0460d77dee5d44fdd
Aaron Christiansen <aaronc20000@gmail.com>
2019-01-03 01:09:12 +0000

    Add object ID property

--- src/element_framework/element.rb
+++ src/element_framework/element.rb
@@ -146,6 +146,7 @@ module LibreFrame
       def properties
         [
           # TODO: Join these together like position
+          UI::Property.from_attribute('ID', self, :do_object_id),
           UI::Property.from_attribute('Width', self, :width),
           UI::Property.from_attribute('Height', self, :height),
           UI::Property.from_attribute('Position', self, :absolute_position),
