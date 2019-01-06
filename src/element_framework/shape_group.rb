af240d76b461aad71e4f0a8e1ceb4b92d44c4c5c
Aaron Christiansen <aaronc20000@gmail.com>
2019-01-06 22:23:37 +0000

    Flesh out ShapeGroup

--- src/element_framework/shape_group.rb
+++ src/element_framework/shape_group.rb
@@ -1,3 +1,5 @@
+require 'clipper'
+
 require_relative 'element'
 
 module LibreFrame
@@ -5,6 +7,37 @@ module LibreFrame
     # A group of other elements which are drawn on the same "plane" using
     # boolean operations. This enables the shapes to blend together.
     class ShapeGroup < StyledElement
+
+      def draw_child_paths?
+        false
+      end 
+
+      def drawing_paths
+        # Prepare a clipper instance
+        clipper = Clipper::Clipper.new
+        current_subject_paths = []
+      
+        children.each do |child|
+          # Load subject polygons
+          current_subject_paths.each do |subject_path|
+            clipper.add_subject_polygon(subject_path)
+          end
+
+          # Load this ShapePath and use it to clip
+          child.drawing_paths.each do |clip_path|
+            clipper.add_clip_polygon(clip_path.map { |pt| [pt.x, pt.y] })
+          end
+
+          # TODO: Make this actually detect op
+          current_subject_paths = clipper.union
+
+          clipper.clear!
+        end
+
+        current_subject_paths.map do |path|
+          path.map { |pt| Core::Point.new(*pt) }
+        end
+      end
     end
   end
 end
